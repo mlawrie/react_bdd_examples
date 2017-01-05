5fad8c9a10eb1e81006413f98061f984a27d3545
Dan LaFeir <danlafeir@gmail.com>
2017-01-04 21:44:04 -0500

    Dan | Adding Semi colons

--- unit_test_example.js
+++ unit_test_example.js
@@ -11,9 +11,9 @@ import { AccountLoginService } from 'accountLoginService';
 describe('AccountLoginView - Traditional Unit Test', () => {
   it('shows the login form section when pressed', () => {
     const instance = shallow(<AccountDetailsView/>).instance();
-    expect(instance.getState().loginFormVisible).to.eq(false)
-    instance.onLoginButton()
-    expect(instance.getState().loginFormVisible).to.eq(true)
+    expect(instance.getState().loginFormVisible).to.eq(false);
+    instance.onLoginButton();
+    expect(instance.getState().loginFormVisible).to.eq(true);
   });
 
   it('adds error messages for submitting an empty form', () => {
@@ -29,7 +29,7 @@ describe('AccountLoginView - Traditional Unit Test', () => {
     jest.setMock('accountLoginService', accountLoginServiceStub);
     const instance = shallow(<AccountDetailsView/>).instance();
     instance.onLoginButton();
-    instance.setState({username: 'jsmith', password: 'password'})
+    instance.setState({username: 'jsmith', password: 'password'});
     instance.onSubmit();
     expect(accountLoginServiceStub).to.be.calledWith('jsmith:password');
   });
@@ -39,8 +39,8 @@ describe('AccountDetailsView - Traditional Unit Test', () => {
   it("shows the user's name and email", () => {
     const instance = shallow(<AccountDetailsView/>).instance();
     instance.setState({firstName: 'John', lastName: 'Smith', email: "jsmith@example.com"});
-    expect(wrapper.text()).to.contain('Name: John Smith')
-    expect(wrapper.text()).to.contain('Email: jsmith@example.com')
+    expect(wrapper.text()).to.contain('Name: John Smith');
+    expect(wrapper.text()).to.contain('Email: jsmith@example.com');
   });
 });
 
@@ -66,13 +66,13 @@ describe('AccountLoginView - Behavioral Component Test', () => {
 
     expect(accountLoginServiceStub).to.be.calledWith('jsmith:password');
   });
-})
+});
 
 describe('AccountDetailsView - Behavioral Component Test', () => {
   it("shows the user's name and email", () => {
-    dispatch(setUserInfoAction({firstName: "John", lastName: "Smith", email: "jsmith@example.com"}))
+    dispatch(setUserInfoAction({firstName: "John", lastName: "Smith", email: "jsmith@example.com"}));
     const wrapper = shallow(<AccountDetailsView/>);
-    expect(wrapper.text()).to.contain('Name: John Smith')
-    expect(wrapper.text()).to.contain('Email: jsmith@example.com')
+    expect(wrapper.text()).to.contain('Name: John Smith');
+    expect(wrapper.text()).to.contain('Email: jsmith@example.com');
   });
-})
+});
