eccc89c11ddefed843bccb681670eb45a75ae3f2
Dan LaFeir <danlafeir@gmail.com>
2017-01-04 21:48:15 -0500

    Dan | Adding Semi colons

--- functional_test_example.js
+++ functional_test_example.js
@@ -1,25 +1,25 @@
 'use strict'
 
-import * as React from 'react'
-import { LoginApp } from 'app'
-import { mount } from 'enzyme'
+import * as React from 'react';
+import { LoginApp } from 'app';
+import { mount } from 'enzyme';
 
-const waitForAsyncTasks = () => new Promise((resolve) => setImmediate(resolve))
-const fillIn = (wrapper, input, value) => wrapper.find(input).simulate('change', {target: {value}})
+const waitForAsyncTasks = () => new Promise((resolve) => setImmediate(resolve));
+const fillIn = (wrapper, input, value) => wrapper.find(input).simulate('change', {target: {value}});
 const lastCallToEndpoint = (httpRequest, url) => {
   const calls = httpRequest.args.filter((args) => args[0] === url);
   if (calls.length == 0) {
-      throw url + ' was never called!'
+      throw url + ' was never called!';
   }
-  return calls[0][1]
-}
+  return calls[0][1];
+};
 
 describe('LoginApp - Subcutaneous Function Test', () => {
-  let httpRequest
+  let httpRequest;
 
   beforeEach(() => {
-    httpRequest = sinon.stub()
-    jest.setMock('http_request', httpRequest)
+    httpRequest = sinon.stub();
+    jest.setMock('http_request', httpRequest);
 
     httpRequest.withArgs(ENDPOINTS.LOGIN, sinon.match.string).returns(Promise.resolve(
       {
@@ -28,22 +28,22 @@ describe('LoginApp - Subcutaneous Function Test', () => {
           last_name: 'Smith',
           email: 'jsmith@example.com'
       }
-    }))
-  })
+    }));
+  });
 
   pit('logs user in and displays account information', () => {
-    const wrapper = mount(<LoginApp/>)
+    const wrapper = mount(<LoginApp/>);
 
-    wrapper.find('#login').simulate('click')
-    fillIn(wrapper, '#username', 'jsmith1')
-    fillIn(wrapper, '#password', 'password1')
-    wrapper.find('#submit').simulate('click')
+    wrapper.find('#login').simulate('click');
+    fillIn(wrapper, '#username', 'jsmith1');
+    fillIn(wrapper, '#password', 'password1');
+    wrapper.find('#submit').simulate('click');
 
     return waitForAsyncTasks().then(() => {
       expect(lastCallToEndpoint(httpRequest, ENDPOINTS.LOGIN).headers.Authorization).to.eql(httpBasic('jsmith1', 'password1'));
 
       expect(wrapper.text()).to.contain('Name: John Smith');
       expect(wrapper.text()).to.contain('Email: jsmith@example.com');
-    })
-  })
-})
+    });
+  });
+});
