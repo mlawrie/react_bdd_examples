353322b2bc7177ecef361c7ba9b173e12955ef8f
Dan LaFeir <danlafeir@gmail.com>
2017-01-27 12:27:16 -0600

    Dan | Remove service testing

--- unit_test_examples/account_login_view-test.js
+++ unit_test_examples/account_login_view-test.js
@@ -3,9 +3,8 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { dispatch } from 'redux_store';
-import { setLoginInfoAction, setUserInfoAction, showLoginFormAction } from 'actions';
+import { showLoginFormAction } from 'actions';
 import { AccountLoginView } from 'accountLoginView';
-import { AccountLoginService } from 'accountLoginService';
 
 describe('AccountLoginView - Traditional Unit Test', () => {
   it('shows the login form section when pressed', () => {
@@ -22,16 +21,6 @@ describe('AccountLoginView - Traditional Unit Test', () => {
     expect(instance.getState().errors).to.contain("Username required");
     expect(instance.getState().errors).to.contain("Password required");
   });
-
-  it("calls AccountLoginService with user's credentials", () => {
-    const accountLoginServiceStub = sinon.stub();
-    jest.setMock('accountLoginService', accountLoginServiceStub);
-    const instance = shallow(<AccountDetailsView/>).instance();
-    instance.onLoginButton();
-    instance.setState({username: 'jsmith', password: 'password'});
-    instance.onSubmit();
-    expect(accountLoginServiceStub).to.be.calledWith('jsmith:password');
-  });
 });
 
 describe('AccountLoginView - Behavioral Component Test', () => {
@@ -43,15 +32,5 @@ describe('AccountLoginView - Behavioral Component Test', () => {
 
     expect(instance.text()).to.contain("Username required");
     expect(instance.text()).to.contain("Password required");
-  });
-
-  it("calls AccountLoginService with user's credentials", () => {
-    const wrapper = shallow(<AccountDetailsView/>);
-    dispatch(showLoginFormAction({loginFormVisible:true}));
-    dispatch(setLoginInfoAction({username: 'jsmith', password: 'password'}));
-
-    wrapper.find('#submit').simulate('click');
-
-    expect(accountLoginServiceStub).to.be.calledWith('jsmith:password');
   });
 });
