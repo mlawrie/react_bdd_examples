3a3b00d895c9b8ca0009428f1425f92cde3c6fd0
dlaffy <dlaffy@users.noreply.github.com>
2017-03-21 21:55:38 -0500

    Dan | Add JSON response to server

--- signupApp/server.js
+++ signupApp/server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const port = 1337;
+const accountDetails = {
+  name: 'First Name',
+  email: 'user@email.com'
+};
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end(); 
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(accountDetails)); 
 });
 server.listen(port, function () {
   console.log("Server listening on: http://localhost:%s", port);
