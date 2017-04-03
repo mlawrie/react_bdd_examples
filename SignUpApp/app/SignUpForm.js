f3dc6cb8a7bf0b2be0f36863a1ab94e3da47ea55
dlaffy <dlaffy@users.noreply.github.com>
2017-04-03 14:25:14 -0400

    Dan | Remove 'working ahead' code

--- SignUpApp/app/SignUpForm.js
+++ SignUpApp/app/SignUpForm.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+const basicValidation = (message) => (field) => ({value: '', error: "Missing Name" });
 
-
-const emptyField = (message) => (field) => console.log(field) && field.value === '' ? { value: field.value, error: message } : field  
 const validate = (validators, formField) => validators.reduce(
-  (field, validator) => console.log(validator(formField.value)) && (Object.assign({}, field, validator(formField.value))), 
+  (field, validator) => (console.log(Object.assign({}, field, validator(formField.value)))), 
   formField  
 );
 
-// const nameValidation1 = (formField) => validate([emptyField("Missing Name")], formField);
-
-const nameValidation = () => ({name: { value: '',  error: 'Missing Name' }})
+const nameValidation = (formField) => validate([basicValidation("")], formField);
 
 const SignUpForm = ({ form, updateValue }) => (
   <form>
-      <input onChange={(evt) => updateValue(nameValidation({value: evt.target.value}))} type='text' placeholder='Name' id='name' />
+      <input type='text' placeholder='Name' id='name' />
       <label>{form.name.error}</label>
       <input type='text' placeholder='Email' id='email' />
       <label>{form.email.error}</label>
@@ -23,7 +20,7 @@ const SignUpForm = ({ form, updateValue }) => (
       <label>{form.password.error}</label>
       <input type='text' placeholder='Confirm Password' id='passwordConfirmation' />
       <label>{form.passwordConfirmation.error}</label>
-      <button onClick={() => updateValue(nameValidation(form.name))} id="submitFormBtn"> Submit </button>
+      <button onClick={() => updateValue({ name: nameValidation(form.name)})} id="submitFormBtn"> Submit </button>
   </form>
 );
 const mapStateToProps = (state) => ({form: state.form});
